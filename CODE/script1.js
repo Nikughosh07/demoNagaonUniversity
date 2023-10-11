a08d69e89f448d31440e51d179b7de4e4b5017bf
Niku Ghosh <147630574+Nikughosh07@users.noreply.github.com>
2023-10-11 20:01:20 +0530

    Add files via upload

---
+++ CODE/script1.js
@@ -0,0 +1,42 @@
+$(document).ready(function(){
+$(".invisible-content").hide();
+$(document).on('click', "#btn",function(){
+var moreLessButton=$(".invisible-content").is(":visible")?'View More':'View Less';
+$(this).text(moreLessButton);
+$(this).parent(".box").find(".invisible-content").toggle();
+$(this).parent(".box").find(".visible-content").toggle();
+});
+});
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("demo");
+  let captionText = document.getElementById("caption");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+  captionText.innerHTML = dots[slideIndex-1].alt;
+}
+
+
